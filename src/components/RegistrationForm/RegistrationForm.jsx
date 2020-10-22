281cce8dbaf616f7a7f639af670d4183a8b65752
Ana Caroline Sinigaglia <ana.sinigaglia@iteris.com.br>
2020-10-22 12:08:25 -0300

    Setting the conditional to start using transitions in the form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import DeliveryData from "../DeliveryData/DeliveryData";
 import PersonalData from "../PersonalData/PersonalData";
 import UserData from "../UserData/UserData";
 
 function RegistrationForm(onSendForm, validateCPF) {
+  const [currentStage, setCurrentStage] = useState(0);
+
+  function currentForm(stage) {
+    switch (stage) {
+      case 0:
+        return <UserData />;
+      case 1:
+        return <PersonalData onSend={onSendForm} validateCPF={validateCPF} />
+      case 2:
+        return <DeliveryData />;
+      default:
+        return <Typography>Erro ao selecionar formulário.</Typography>
+    }
+  }
 
   return (
     <div>
-      <PersonalData onSend={onSendForm} validateCPF={validateCPF} />
-      <UserData />
-      <DeliveryData />
+      { currentForm(currentStage) }
     </div>
   );
 }
