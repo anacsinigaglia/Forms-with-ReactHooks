b6710e91e7c41cace2c9b2e9e8732cff420f5253
Ana Caroline Sinigaglia <anacarolinesinigaglia@gmail.com>
2020-10-26 11:22:23 -0300

    Password validation

--- forms-halliday/src/pages/Home.tsx
+++ forms-halliday/src/pages/Home.tsx
@@ -1,48 +1,89 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import ReCAPTCHA from "react-google-recaptcha";
-import { FormData } from './interfaces/FormData';
-import './Home.scss';
+import { FormData } from "./interfaces/FormData";
+import "./Home.scss";
 
 function Home() {
-    const {register, handleSubmit, errors} = useForm<FormData>();
-    return (
+  const { register, handleSubmit, errors } = useForm<FormData>();
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit((formData) => {
+          /*onSubmit é um event listener */
+          console.log(formData, "formData");
+        })}
+      >
         <div>
-            <form onSubmit={handleSubmit((formData) => {
-                console.log(formData, "formData")
-            })}> {/*onSubmit é um event listener */}
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" ref={register({required: "required"})} /> 
-                {/* ref=register() funciona para vincular a function com esse input */}
-                {/* quando colocamos "required" no required, é a mensagem que aparecerá. poderia constar só true */}
-                    {errors.name ? <div>{errors.name.message}</div> : null}
-                </div>
-
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" ref={register({required: "required"})} />
-                    {errors.email ? <div>{errors.email.message}</div> : null}
-                </div>
-                
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" ref={register({required: "required"})} />
-                    {errors.password ? <div>{errors.password.message}</div> : null}
-                </div>
-                
-                <div>
-                    <input type="checkbox" name="terms" id="terms" ref={register({required: "you must agree to our terms"})} />
-                    <label htmlFor="terms">Accept the terms</label>
-                    {errors.terms ? <div>{errors.terms.message}</div> : null}
-                </div>
-
-                <div>
-                    <button type="submit">Register</button>
-                </div>
-            </form>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            ref={register({ /* ref=register() funciona para vincular a function (do useForm) com esse input */
+              required: "required" /* quando colocamos "required" no required, é a mensagem que aparecerá. poderia ser só true */,
+            })}
+          />
+          {errors.name ? <div>{errors.name.message}</div> : null}{" "}
+          {/*setamos a msg para realmente ser renderizada caso exista o erro*/}
         </div>
-    );
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            ref={register({ required: "required" })}
+          />
+          {errors.email ? <div>{errors.email.message}</div> : null}
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            ref={register({
+              required: "required",
+              minLength: {
+                value: 8,
+                message: "must be 8 characters",
+              },
+                 validate: (value) => {
+                    return (
+                    [/[a-z]/, 
+                     /[A-Z]/, 
+                     /[0-9]/, 
+                     /[^a-zA-Z0-9]/
+                    ].every((pattern) => pattern.test(value)) 
+                        || "Must include lower, upper, number and special characters."
+                    );
+                },
+            })}
+          />
+          {errors.password ? <div>{errors.password.message}</div> : null}
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="terms"
+            id="terms"
+            ref={register({ required: "you must agree to our terms" })}
+          />
+          <label htmlFor="terms">Accept the terms</label>
+          {errors.terms ? <div>{errors.terms.message}</div> : null}
+        </div>
+
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
